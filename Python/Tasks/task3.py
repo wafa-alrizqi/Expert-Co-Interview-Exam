cf7bd12dd20dac1128a2b54bb6fc492460135ab9
wafa-alrizqi <wafa.alrizqi@gmail.com>
2022-09-27 15:22:25 +0300

    code cleaned and organized

--- Python/Tasks/task3.py
+++ Python/Tasks/task3.py
@@ -1,27 +1,27 @@
 # Task 3:
-    # Create a running Python project to check if all digits of a number divide it
-    # given a number n, find whether all digits of n divide it or not.
+# Given a number n, check if whether all digits of n divide it or not.
 
 
-# the list comprehension used to split the number into digits .
-# Strings are Arrays, so if i convert it to string i can split the number easily
-
-user_input = int(input('Enter a Number: '))
-x = [int(a) for a in str(user_input)]
-
-def divisibility_check(number_list:list):
+def divisibility_check(number_list: list):
     """
-    function to check if the number is divide by its all digit or not 
+    Function to determine whether or not a number has been divided by all of its digits.
     """
     try:
         for i in number_list:
-            if user_input % i == 0 :
-                print(user_input ,'%' , i , '== 0')
+            if user_input % i == 0:
+                print(user_input, '%', i, '== 0')
                 continue
             else:
                 print('Not divisible by its all digits')
                 break
     except ZeroDivisionError as e:
         print(e)
+
+
+user_input = int(input('Enter a Number: '))
+
+# the list comprehension used to split the number into digits .
+# Strings are Arrays, so if i convert it to string i can split the number easily
+x = [int(a) for a in str(user_input)]
 
 divisibility_check(x)
